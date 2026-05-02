a664b43c805629c52fed62ea8e1115fdac830824
carlosarturoyepescelon11-boop <carlosarturoyepescelon11@gmail.com>
2026-05-02 15:29:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -501,4 +501,5 @@ def historial_caja():
     return render_template("historial_caja.html", historial=historial)
 
 if __name__ == "__main__":
+    init_db()
     app.run(host="0.0.0.0", port=5000, debug=True)
